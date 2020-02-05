9837b948aed12744f2a52b2d6ca21a66d12579e6
FelipeCorerato <felipe.corerato@ifood.com.br>
2020-02-05 10:58:45 -0300

    Add remove character feature

--- src/reducers/characters_reducer.js
+++ src/reducers/characters_reducer.js
@@ -1,10 +1,16 @@
-import { ADD_CHARACTER } from '../actions';
+import { ADD_CHARACTER, REMOVE_CHARACTER } from '../actions';
+
 import characters_json from '../data/characters.json';
+import { createCharacter } from './helpers';
 
 function characters(state = characters_json, action) {
+    let characters;
     switch (action.type) {
         case ADD_CHARACTER:
-            let characters = state.filter(item => item.id !== action.id);
+            characters = state.filter(item => item.id !== action.id);
+            return characters;
+        case REMOVE_CHARACTER: 
+            characters = [...state, createCharacter(action.id)];
             return characters;
         default:
             return state;
