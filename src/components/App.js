5cc938f17761287b425ee16707eb416f770ac575
FelipeCorerato <felipe.corerato@ifood.com.br>
2020-02-05 11:00:36 -0300

    Import styles and add the new components

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,30 @@
+import '../styles/index.css';
+
 import CharactersList from './CharactersList';
+import HeroesList from './HeroesList';
 import React from 'react';
+import SquadStats from './SquadStats';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h2>Supersquad</h2>
-        <CharactersList />
+      <div className="App">
+        <h2>SuperSquad</h2>
+
+        <div className="col-md-4">
+          <CharactersList />
+        </div>
+        
+        <div className="col-md-4">
+          <HeroesList />
+        </div>
+
+        <div className="col-md-4">
+          <SquadStats />
+        </div>
       </div>
     );
   }
-  
 }
 
 export default App;
